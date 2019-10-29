b355efaa706090c42177c8345b49625d12a065a6
Nicholas Priambodo <nicholaspriambodo@gmail.com>
2019-10-29 08:30:28 +0700

    change resource color and restructure package to mvvm

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nicholas_priambodo/public_mail_by_nicho/view/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nicholas_priambodo/public_mail_by_nicho/view/MainActivity.java
@@ -1,8 +1,10 @@
-package id.ac.ui.cs.mobileprogramming.nicholas_priambodo.public_mail_by_nicho;
+package id.ac.ui.cs.mobileprogramming.nicholas_priambodo.public_mail_by_nicho.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import id.ac.ui.cs.mobileprogramming.nicholas_priambodo.public_mail_by_nicho.R;
 
 public class MainActivity extends AppCompatActivity {
 
