069c5016b18454e10f5ff50baf89746fe1b28b91
Nicholas Priambodo <nicholaspriambodo@gmail.com>
2019-12-25 16:37:54 +0700

    added Java Native Interface setting

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,7 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jint JNICALL
+Java_id_ac_ui_cs_mobileprogramming_nicholas_1priambodo_public_1mail_1by_1nicho_view_LoginActivity_testFromJNI(JNIEnv *env, jobject) {
+    return 1;
+}
