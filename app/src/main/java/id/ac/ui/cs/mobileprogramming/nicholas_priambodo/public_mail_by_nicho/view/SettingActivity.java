2a4c9616e92ca1a380972518c777d97df1343928
Nicholas Priambodo <nicholaspriambodo@gmail.com>
2019-12-26 12:40:32 +0700

    create runtime permission for WRITE_EXTERNAL_STORAGE

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nicholas_priambodo/public_mail_by_nicho/view/SettingActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nicholas_priambodo/public_mail_by_nicho/view/SettingActivity.java
@@ -1,11 +1,26 @@
 package id.ac.ui.cs.mobileprogramming.nicholas_priambodo.public_mail_by_nicho.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import id.ac.ui.cs.mobileprogramming.nicholas_priambodo.public_mail_by_nicho.R;
 import id.ac.ui.cs.mobileprogramming.nicholas_priambodo.public_mail_by_nicho.viewmodel.SettingViewModel;
@@ -14,6 +29,7 @@ public class SettingActivity extends AppCompatActivity {
     private final int TEN_SECONDS = 10000;
     private final int THIRTY_SECONDS = 30000;
     private final int SIXTY_SECONDS = 60000;
+    private final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
     private RadioGroup radioGroup;
     private SettingViewModel settingViewModel;
 
@@ -79,6 +95,55 @@ public class SettingActivity extends AppCompatActivity {
             settingViewModel.updateRefreshTime(refresh_time);
 
             return null;
+        }
+    }
+
+    public void onClickSave(View view) {
+        if (write_external_storage_is_allowed()) {
+            //TODO
+        }
+    }
+
+    private boolean write_external_storage_is_allowed() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(getResources().getString(R.string.write_external_storage_permission_explanation_title))
+                    .setMessage(getResources().getString(R.string.write_external_storage_permission_explanation_message))
+                    .setPositiveButton(
+                            android.R.string.ok,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    requestWriteExternalStoragePermission();
+                                }
+                            }
+                    )
+                    .create()
+                    .show();
+        } else {
+            requestWriteExternalStoragePermission();
+        }
+        return false;
+    }
+
+    private void requestWriteExternalStoragePermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                onClickSave(null);
+            } else {
+                Toast.makeText(this, getResources().getString(R.string.write_external_storage_permission_not_allow), Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
